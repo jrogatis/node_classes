8e70e360f14ec8db6342371ea43b462a3f5f43a1
Jean Philip de Rogatis <jrogatis@metaconexao.com.br>
2018-12-16 13:40:40 -0200

    feat: token service

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
+const config = require('./lib/config');
 const fs = require('fs');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
 // Instantiate the HTTP server
 const httpServer = http.createServer((req, res) => {
@@ -74,16 +76,16 @@ const unifiedServer = (req, res) => {
       queryStringObject: queryStringObject,
       method: method,
       headers: headers,
-      payload: buffer
+      payload: helpers.parseJsonToObject(buffer)
     };
 
     // Route the request to the handler specified in the router
     chosenHandler(data, (statusCode, payload) => {
       // Use the status code returned from the handler, or set the default status code to 200
-      statusCode = typeof statusCode == 'number' ? statusCode : 200;
+      statusCode = typeof statusCode === 'number' ? statusCode : 200;
 
       // Use the payload returned from the handler, or set the default payload to an empty object
-      payload = typeof payload == 'object' ? payload : {};
+      payload = typeof payload === 'object' ? payload : {};
 
       // Convert the payload to a string
       const payloadString = JSON.stringify(payload);
@@ -97,20 +99,9 @@ const unifiedServer = (req, res) => {
   });
 };
 
-// Define all the handlers
-const handlers = {};
-
-// Ping handler
-handlers.ping = (data, callback) => {
-  callback(200);
-};
-
-// Not-Found handler
-handlers.notFound = (data, callback) => {
-  callback(404);
-};
-
 // Define the request router
 const router = {
-  ping: handlers.ping
+  ping: handlers.ping,
+  users: handlers.users,
+  tokens: handlers.tokens
 };
